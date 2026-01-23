cfd558ee121b13026c9b66a664f33ffc59531135
praveena <praveenapalla4@gmail.com>
2026-01-23 15:20:54 +0530

    40-databases

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -19,7 +19,7 @@ data "aws_ami" "joindevops" {
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
-  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+  name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
 
 data "aws_ssm_parameter" "database_subnet_ids" {
