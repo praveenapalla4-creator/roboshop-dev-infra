13b60039d06218451ca7c7bd82566c9771532c03
praveena <praveenapalla4@gmail.com>
2026-01-23 15:02:30 +0530

    40-databases

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mongodb" {
   ami           =local.ami_id
   instance_type = "t3.micro"
   vpc_security_group_ids =[local.mongodb_sg_id]
-  subnet_id=local.databse_subnet_ids
+  subnet_id=local.database_subnet_ids
   tags = merge(
     local.common_tags,{
         Name="${local.common_name_suffix}-mongodb" #roboshop-dev-mongodb
