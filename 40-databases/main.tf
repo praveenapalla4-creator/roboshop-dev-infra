ccefcb8f1246b26965b760670e747ae2d5bf1799
praveena <praveenapalla4@gmail.com>
2026-01-24 19:21:36 +0530

    route53

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -165,3 +165,36 @@ destination = "/tmp/bootstrap.sh" # Destination on EC2
     ]
   }
 }
+
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = var.zone_id
+  name    = "mongodb-${var.environment}.${var.domain_name}" #mongodb-dev.daws86s.help
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}" #mongodb-dev.daws86s.help
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}.${var.domain_name}" #mongodb-dev.daws86s.help
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}" #mongodb-dev.daws86s.help
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+}
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -4,3 +4,10 @@ variable "project_name" {
 variable "environment" {
     default="dev"
 }
+
+variable "zone_id"{
+    default="Z0398365AXZ3ME1MH9EB"
+}
+variable "domain_name"{
+    default="daws86s.help"
+}
