6fd6d104e963032325c246c837757c4271983ce3
praveena <praveenapalla4@gmail.com>
2026-01-24 10:07:12 +0530

    databases bootsrap.sh

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 dnf install ansible -y
-ansible-pull -U https://github.com/praveenapalla4-creator/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+# ansible-pull -U https://github.com/praveenapalla4-creator/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 # git clone ansible-playbook
 #cd ansible-playbook
 #ansible-playbook -i inventory main.yaml
-# REPO_URL=https://github.com/praveenapalla4-creator/ansible-roboshop-roles-tf.git
-# REPO_DIR=/opt/roboshop/ansible
-# ANSIBLE_DIR=ansible-roboshop-roles-tf
+REPO_URL=https://github.com/praveenapalla4-creator/ansible-roboshop-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-tf
+
 component=$1
-# mkdir -p $REPO_DIR
-# mkdir -p /var/log/roboshop/
-# touch ansible.log
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+touch ansible.log
 
 
-# cd $REPO_DIR
-# #chekc if anisible repo is already closed or not
+cd $REPO_DIR
+#chekc if anisible repo is already closed or not
 
-# if [ -d $ANSIBLE_DIR ]; then
+if [ -d $ANSIBLE_DIR ]; then
   
-#   cd $ANSIBLE_DIR
-#   git pull
-# else 
-#     git clone $REPO_URL
-#     cd $ANSIBLE_DIR
-# fi
+  cd $ANSIBLE_DIR
+  git pull
+else 
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
 
-# ansible-playbook -e component =$component main.yaml
+ansible-playbook -e component=$component main.yaml
