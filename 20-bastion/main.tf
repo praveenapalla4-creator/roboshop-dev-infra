a2b2016a4fbfa5e4397cd5b9727e7ba02bc38ffc
praveena <praveenapalla4@gmail.com>
2026-01-24 16:42:35 +0530

    mysql environmnet

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "bastion" {
   subnet_id=local.public_subnet_id
   iam_instance_profile=aws_iam_instance_profile.bastion.name
   user_data= file("bastion.sh") 
-   
+
   tags = merge( 
     local.common_tags,{
         Name="${var.project_name}-${var.environment}-bastion"
@@ -15,5 +15,5 @@ resource "aws_instance" "bastion" {
 
 resource "aws_iam_instance_profile" "bastion" {
 name = "bastion"
-role = "BastionTerraformAdmin"
+role = "BastionTerraformAdminn"
 }
