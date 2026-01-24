1a4d4285c939adc6eb4da484875242a8f4c3a97d
praveena <praveenapalla4@gmail.com>
2026-01-24 19:22:44 +0530

    route53

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -173,6 +173,7 @@ resource "aws_route53_record" "mongodb" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "redis" {
@@ -181,6 +182,7 @@ resource "aws_route53_record" "redis" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "mysql" {
@@ -189,6 +191,7 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "rabbitmq" {
@@ -197,4 +200,5 @@ resource "aws_route53_record" "rabbitmq" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
 }
