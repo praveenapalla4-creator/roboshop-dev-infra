f6be2d1db0bdb5c5bd05cc5ce38d04b7202605ed
praveena <praveenapalla4@gmail.com>
2026-01-26 15:53:00 +0530

    acm and frontend

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -142,7 +142,7 @@ resource "aws_autoscaling_group" "catalogue" {
     preferences {
       min_healthy_percentage = 50
     }
-    triggers = ["aws_launch_template"]
+    triggers = ["launch_template"]
   }
 
 
